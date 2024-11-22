e56980649a7f1b9880cb53c84207e3c90e74518c
Wollacy <carvalho_1990@outlook.com>
2024-11-22 13:26:31 -0300

    Comit dia 05

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../dbConfig.js";
 
 // Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
@@ -19,4 +20,12 @@ export async function getTodosPosts() {
     const colecao = db.collection("posts");
   
     return colecao.insertOne(novoPost);
+  }
+
+  export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set: novoPost});
   }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,13 @@
+import 'dotenv/config';
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import cors from "cors";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,12 +23,19 @@ const upload = multer({ dest: "./uploads" , storage})
 const routes = (app) => {
     // Permite que o Express entenda requisições JSON
     app.use(express.json());
+    app.use(cors(corsOptions));
 
     // Rota para obter todos os posts
     app.get("/posts", listarPosts);
+
     // Rota para criar um novo post
     app.post("/posts", postarNovoPost);
+
+    // Rota para criar uma nova imagem
     app.post("/upload", upload.single("imagem"), uploadImagem);
+
+    // Rota para atualizar um post
+    app.put("/upload/:id", atualizarNovoPost);
 }
 
 export default routes;
